4e510b7ae8f9e1d28b24da379ce55ffbc39fa7e1
Woze Parrot <wozeparrot@gmail.com>
2024-05-02 16:19:11 -0700

    feat: more robust handling

--- tinyturing/display.py
+++ tinyturing/display.py
@@ -1,4 +1,4 @@
-import math, os
+import math, os, subprocess, time
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import serial, pygame
 
@@ -16,7 +16,7 @@ QUERY_STATUS = bytearray([0xcf, 0xef, 0x69, 0x00, 0x00, 0x00, 0x01])
 WIDTH, HEIGHT = 800, 480
 class Display:
   def __init__(self, port):
-    self.lcd = serial.Serial(port, 115200, timeout=5, write_timeout=5)
+    self.lcd = serial.Serial(port, 115200, timeout=2, write_timeout=2)
     os.environ["SDL_VIDEODRIVER"] = "dummy"
     pygame.init()
     pygame.font.init()
@@ -35,18 +35,23 @@ class Display:
   def __del__(self): self.lcd.close()
 
   def send_command(self, command, payload=None):
+    print(f"Sending command {command}")
     if payload is not None: command += payload
     padding = 0 if command[0] != 0x2c else 0x2c
     if not ((cmd_len:=len(command)) / 250).is_integer(): command += bytearray([padding] * (250 * math.ceil(cmd_len / 250) - cmd_len))
     try: self.lcd.write(command)
     except serial.SerialTimeoutException:
-      self.lcd.reset_input_buffer()
+      print("Serial write timeout, resetting usb device and retrying")
+      subprocess.run(["usbreset", "1d6b:0106"])
+      time.sleep(2)
+      self.lcd = serial.Serial(self.lcd.port, self.lcd.baudrate, timeout=5, write_timeout=5)
       self.lcd.write(command)
 
   def text(self, text, size, *args, **kwargs):
     if size not in self.font_cache: self.font_cache[size] = pygame.font.Font(None, size)
     return self.font_cache[size].render(text, *args, **kwargs)
   def blit(self, source, dest=(0, 0), area=None):
+    print(f"Blitting {source.get_width()}x{source.get_height()} image at {dest} with area {area}")
     self.framebuffer.blit(source, dest, area)
     if area is None: self.framebuffer_dirty[dest[1]:dest[1]+source.get_height()][dest[0]:dest[0]+source.get_width()] = [[True] * source.get_width() for _ in range(source.get_height())]
     else: self.framebuffer_dirty[dest[1]:dest[1]+area.height][dest[0]:dest[0]+area.width] = [[True] * area.width for _ in range(area.height)]
@@ -55,11 +60,14 @@ class Display:
     self.framebuffer_dirty = [[True] * WIDTH for _ in range(HEIGHT)]
 
   def flip(self):
-    if not any(any(row) for row in self.framebuffer_dirty): return
+    if not any(any(row) for row in self.framebuffer_dirty):
+      print("Skipping flip because framebuffer is clean")
+      return
+    print("Flipping framebuffer")
     self.send_command(PRE_UPDATE_BITMAP)
     self.send_command(START_DISPLAY_BITMAP)
     self.send_command(DISPLAY_BITMAP)
     framebuffer = pygame.surfarray.array2d(self.framebuffer).transpose().tobytes()
     self.send_command(bytearray([0xff]), b"\x00".join([framebuffer[i:i+249] for i in range(0, len(framebuffer), 249)]))
-    self.lcd.read(1024)
+    print(self.lcd.read(1024)[:0x20])
     self.framebuffer_dirty = [[False] * WIDTH for _ in range(HEIGHT)]
