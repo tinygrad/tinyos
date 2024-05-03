e5740f5e997054171a8054f7023d0252e8012735
Woze Parrot <wozeparrot@gmail.com>
2024-05-03 14:42:07 -0700

    feat: factor out

--- tinyturing/display.py
+++ tinyturing/display.py
@@ -2,7 +2,7 @@ import math, os, subprocess, time
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import serial, pygame
 import numpy as np
-from numba import jit, njit
+from numba import njit
 
 # commands
 HELLO = bytearray([0x01, 0xef, 0x69, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0xc5, 0xd3])
@@ -12,7 +12,7 @@ SET_BRIGHTNESS = bytearray([0x7b, 0xef, 0x69, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00
 PRE_UPDATE_BITMAP = bytearray([0x86, 0xef, 0x69, 0x00, 0x00, 0x00, 0x01])
 START_DISPLAY_BITMAP = bytearray([0x2c])
 DISPLAY_BITMAP = bytearray([0xc8, 0xef, 0x69, 0x00, 0x17, 0x70])
-UPDATE_BITMAP = bytearray([0xcc, 0xef, 0x69, 0x00])
+UPDATE_BITMAP = bytes([0xcc, 0xef, 0x69, 0x00])
 QUERY_STATUS = bytearray([0xcf, 0xef, 0x69, 0x00, 0x00, 0x00, 0x01])
 
 WIDTH, HEIGHT = 800, 480
@@ -32,7 +32,6 @@ class Display:
     self.font_cache = {}
     self.framebuffer = pygame.Surface((WIDTH, HEIGHT), flags=pygame.SRCALPHA)
     self.old_framebuffer = self.framebuffer.copy()
-    self.framebuffer_dirty = [[True] * WIDTH for _ in range(HEIGHT)]
     self.partial_update_count = 0
 
   def __del__(self): self.lcd.close()
@@ -64,48 +63,27 @@ class Display:
     print(f"[D] Blitting {source.get_width()}x{source.get_height()} image at {dest} with area {area}")
     self.framebuffer.blit(source, dest, area)
 
-  def _track_damage(self):
-    self.framebuffer_dirty = _track_damage(pygame.surfarray.pixels2d(self.old_framebuffer), pygame.surfarray.pixels2d(self.framebuffer))
-
   def flip(self):
-    self._track_damage()
+    dirty = _track_damage(pygame.surfarray.pixels2d(self.old_framebuffer), pygame.surfarray.pixels2d(self.framebuffer))
 
-    if not any(any(row) for row in self.framebuffer_dirty):
+    if not np.any(dirty):
       print("[D] Skipping flip because framebuffer is clean")
       return
 
     # check if the whole framebuffer is dirty
-    if all(all(row) for row in self.framebuffer_dirty):
+    if np.all(dirty):
       print("[D] Flipping full framebuffer")
       self.send_command(PRE_UPDATE_BITMAP)
       self.send_command(START_DISPLAY_BITMAP)
       self.send_command(DISPLAY_BITMAP)
-      framebuffer = pygame.surfarray.array2d(self.framebuffer).transpose().tobytes()
+      framebuffer = pygame.surfarray.pixels2d(self.framebuffer).transpose().tobytes()
       self.send_command(bytearray([0xff]), b"\x00".join([framebuffer[i:i+249] for i in range(0, len(framebuffer), 249)]))
       print(f"[D] {self.lcd.read(1024)[:0x20]}")
       self.send_command(QUERY_STATUS)
       print(f"[D] {self.lcd.read(1024)[:0x20]}")
     else:
       print("[D] Flipping partial framebuffer")
-      update = bytearray()
-      framebuffer = pygame.PixelArray(self.framebuffer)
-      for y in range(HEIGHT):
-        if not any(self.framebuffer_dirty[y]): continue
-        # find first dirty pixel
-        start = 0
-        while not self.framebuffer_dirty[y][start]: start += 1
-        # find last dirty pixel
-        end = WIDTH - 1
-        while not self.framebuffer_dirty[y][end]: end -= 1
-        update += (y * WIDTH + start).to_bytes(3, "big") + (end - start + 1).to_bytes(2, "big")
-        for x in range(start, end + 1):
-          pixel = framebuffer[x, y]
-          update += (pixel & 0xffffff).to_bytes(3, "little")
-      framebuffer.close()
-      update_size = (len(update) + 2).to_bytes(3, "big")
-      payload = UPDATE_BITMAP + update_size + bytearray(3) + self.partial_update_count.to_bytes(4, "big")
-      update = b"\x00".join(update[i:i + 249] for i in range(0, len(update), 249))
-      update += bytearray([0xef, 0x69])
+      update, payload = _update_payload(dirty, pygame.PixelArray(self.framebuffer), self.partial_update_count)
 
       self.send_command(bytearray([0xff]), payload)
       self.send_command(bytearray([0xff]), update)
@@ -128,5 +106,24 @@ class Display:
     self.framebuffer_dirty = [[False] * WIDTH for _ in range(HEIGHT)]
 
 @njit
-def _track_damage(old:np.ndarray, new:np.ndarray):
-  return np.where(old != new, 1, 0).T
+def _track_damage(old:np.ndarray, new:np.ndarray): return np.where(old != new, 1, 0).T
+
+def _update_payload(dirty:np.ndarray, fb, partial_update_count):
+  update = bytearray()
+  for y in range(HEIGHT):
+    if not np.any(dirty[y]): continue
+    # find first dirty pixel
+    start = 0
+    while not dirty[y][start]: start += 1
+    # find last dirty pixel
+    end = WIDTH - 1
+    while not dirty[y][end]: end -= 1
+    update += (y * WIDTH + start).to_bytes(3, "big") + (end - start + 1).to_bytes(2, "big")
+    for x in range(start, end + 1):
+      pixel = fb[x, y]
+      update += (pixel & 0xffffff).to_bytes(3, "little")
+  update_size = (len(update) + 2).to_bytes(3, "big")
+  payload = UPDATE_BITMAP + update_size + b"\x00\x00\x00" + partial_update_count.to_bytes(4, "big")
+  update_chunks = []
+  for i in range(0, len(update), 249): update_chunks.append(update[i:i+249])
+  return b"\x00".join(update_chunks) + b"\xef\x69", payload
