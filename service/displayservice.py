b44caf44fd3074cbd270f0221fe0d7020b4e883f
Woze Parrot <wozeparrot@gmail.com>
2024-05-02 18:52:00 -0700

    feat: no stripping

--- service/displayservice.py
+++ service/displayservice.py
@@ -86,7 +86,7 @@ def display_thread():
 
 class ControlHandler(StreamRequestHandler):
   def handle(self):
-    data = self.rfile.readline().strip().decode()
+    data = self.rfile.readline().decode()
     command, *args = data.split(",")
     print(f"[CH] Received command {command} with args {args}")
     match command:
