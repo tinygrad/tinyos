cf2ccdbb94c9c6b74f3225efd604aae527d8bdb9
Woze Parrot <wozeparrot@gmail.com>
2024-05-03 09:34:18 -0700

    feat: power-limit tool

---
+++ tools/power-limit_completion.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+_power-limit()
+{
+  local cur prev opts
+  COMPREPLY=()
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  prev="${COMP_WORDS[COMP_CWORD-1]}"
+  opts="$(seq 100 350)"
+
+  # only complete the first argument
+  if [[ ${COMP_CWORD} -eq 1 ]] ; then
+    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+    return 0
+  fi
+}
