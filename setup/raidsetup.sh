4de3f0e227f5fdf1308b11b0868dfa8b13abdb58
Woze Parrot <wozeparrot@gmail.com>
2024-05-03 16:42:57 -0700

    feat: better raid?

--- setup/raidsetup.sh
+++ setup/raidsetup.sh
@@ -3,7 +3,7 @@
 echo "atext,Creating RAID.. ,Creating RAID ..,Creating RAID. ." | nc -U /run/tinybox-screen.sock
 
 umount /raid
-mdadm --stop /dev/md0
+mdadm --stop /dev/md127 /dev/md0
 mdadm --zero-superblock /dev/nvme0n1 /dev/nvme1n1 /dev/nvme2n1 /dev/nvme3n1
-mdadm --create /dev/md0 --level=0 --raid-devices=4 /dev/nvme0n1 /dev/nvme1n1 /dev/nvme2n1 /dev/nvme3n1 -f
-mkfs.xfs /dev/md0 -f
+mdadm --create /dev/md127 --level=0 --raid-devices=4 /dev/nvme0n1 /dev/nvme1n1 /dev/nvme2n1 /dev/nvme3n1 -f
+mkfs.xfs /dev/md127 -f
