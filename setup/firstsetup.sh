197d18dde63231b78f924c2cba1dba24d58613b0
Woze Parrot <wozeparrot@gmail.com>
2024-05-03 18:13:30 -0700

    feat: more entropy

--- setup/firstsetup.sh
+++ setup/firstsetup.sh
@@ -13,7 +13,7 @@ function main {
 
   # change password of tiny to something random
   local random_password
-  random_password="$(openssl rand -hex 2)"
+  random_password="$(openssl rand -hex 3)"
   echo "tiny:$random_password" | chpasswd
   passwd --expire tiny
 
