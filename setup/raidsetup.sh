09edd21e51194daea6db3641690ae02f83eecbf0
Woze Parrot <wozeparrot@gmail.com>
2024-05-03 18:02:59 -0700

    feat: no -f

--- setup/raidsetup.sh
+++ setup/raidsetup.sh
@@ -6,4 +6,4 @@ umount /raid
 mdadm --stop /dev/md127 /dev/md0
 mdadm --zero-superblock /dev/nvme0n1 /dev/nvme1n1 /dev/nvme2n1 /dev/nvme3n1
 mdadm --create /dev/md127 --level=0 --raid-devices=4 /dev/nvme0n1 /dev/nvme1n1 /dev/nvme2n1 /dev/nvme3n1 -f
-mkfs.ext4 /dev/md127 -f
+mkfs.ext4 /dev/md127
