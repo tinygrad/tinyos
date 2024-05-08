48c9b2228ff5738e0261672554d032d681036216
Woze Parrot <wozeparrot@gmail.com>
2024-05-08 15:43:38 -0700

    feat: no out of bounds

--- service/displayservice.py
+++ service/displayservice.py
@@ -129,9 +129,9 @@ class LineGraph(Displayable):
     max_data, min_data = max(self.data), min(self.data)
     if max_data == min_data: return
     surface = np.full((self.width, self.height, 3), 0)
-    for i in range(1, len(self.data)):
-      x1, y1 = self.width * (i - 1) // (self.points_to_keep - 1), self.height * (self.data[i - 1] - min_data) // (max_data - min_data)
-      x2, y2 = self.width * i // (self.points_to_keep - 1), self.height * (self.data[i] - min_data) // (max_data - min_data)
+    for i in range(len(self.data)):
+      x1, y1 = int(self.width * i / (self.points_to_keep - 1)), int(self.height * (self.data[i - 1] - min_data) / (max_data - min_data))
+      x2, y2 = int(self.width * i / (self.points_to_keep - 1)), int(self.height * (self.data[i] - min_data) / (max_data - min_data))
       # draw line
       for point in line(x1, y1, x2, y2): surface[point[0], point[1]] = [255, 255, 255]
     display.blit(surface, (self.x - self.width // 2, self.y - self.height // 2))
