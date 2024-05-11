6abd901ada0ee358cd29e1adf24fa7d2052af85e
Woze Parrot <wozeparrot@gmail.com>
2024-05-10 21:09:29 -0700

    feat: not dumb password setup

---
+++ setup/adduser.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -x
+
+# first disable ssh password authentication
+sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
+systemctl restart sshd
+
+adduser tiny
+
+# add user to groups
+usermod -aG sudo tiny
+usermod -aG adm tiny
+usermod -aG audio tiny
+usermod -aG cdrom tiny
+usermod -aG dialout tiny
+usermod -aG floppy tiny
+usermod -aG lxd tiny
+usermod -aG netdev tiny
+usermod -aG plugdev tiny
+usermod -aG video tiny
+usermod -aG render tiny
+
+# set password
+echo "tiny:tiny" | chpasswd
--- setup/scramble.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -x
-
-RANDOM_PASSWORD="$(openssl rand -hex 10)"
-echo "tiny:$RANDOM_PASSWORD" | chpasswd
-passwd --expire tiny
