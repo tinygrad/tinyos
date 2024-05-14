f9efc595673bf7e2d1db72166dab798af9b94007
Woze Parrot <wozeparrot@gmail.com>
2024-05-14 15:34:24 -0700

    feat: change firstsetup

--- setup/firstsetup.sh
+++ setup/firstsetup.sh
@@ -2,23 +2,15 @@
 set -x
 
 function main {
-  # see if the password must be changed to see if we are in first setup
-  if ! passwd -S tiny | grep "1970"; then
-    systemctl disable firstsetup.service
-    exit 0
-  fi
-
   # find our local ip
   local local_ip
   local_ip="$(hostname -I | awk '{print $1}')"
 
-  # change password of tiny to something random
-  local random_password
-  random_password="$(openssl rand -hex 4)"
-  echo "tiny:$random_password" | chpasswd
-  passwd --expire tiny
+  # find the bmc ip
+  local bmc_ip
+  bmc_ip="$(ipmitool lan print | grep "IP Address  " | awk '{print $4}')"
 
-  echo "text,IP: $local_ip,Password: $random_password" | nc -U /run/tinybox-screen.sock
+  echo "text,IP: $local_ip,BMC: $bmc_ip" | nc -U /run/tinybox-screen.sock
 }
 
 main "$@"
