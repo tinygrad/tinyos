52c1517448f256e4ee3f1879b087408fa9824c88
Woze Parrot <wozeparrot@gmail.com>
2024-05-14 11:28:33 -0700

    Squashed 'tinyturing/' changes from 2290654..add68d8
    add68d8 feat: minimize
    7af4ed2 feat: examples
    7812a3a feat: remove logo
    b5374ac feat: fix freezing issues
    9acb1c9 feat: remove pygame
    6377fff feat: current state
    
    git-subtree-dir: tinyturing
    git-subtree-split: add68d8af67b4e959a80545c38938b006803fe63

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; let
+            pythonWithPackages = python3.withPackages (p:
+              with p; [
+                pyserial
+                pillow
+                numpy
+                numba
+              ]);
+          in [
+            pythonWithPackages
+          ];
+        };
+      }
+    );
+}
