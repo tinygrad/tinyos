74a48bd298b1785d15a803ae637ecb44422440a7
Woze Parrot <wozeparrot@gmail.com>
2024-05-16 18:52:17 -0700

    feat: symlink stuff

--- setup/initialpackage.sh
+++ setup/initialpackage.sh
@@ -3,9 +3,16 @@ set -x
 
 echo "atext,Preparing.. ,Preparing ..,Preparing. ." | nc -U /run/tinybox-screen.sock
 
-# change to user tiny
+# clone tinygrad
 su tiny -c "git clone https://github.com/tinygrad/tinygrad /home/tiny/tinygrad"
+
+# install tinygrad and deps
 pushd /home/tiny/tinygrad
 su tiny -c "pip install -e ."
 su tiny -c "pip install pillow"
+
+# symlink datasets and weights
+su tiny -c "ln -s /raid/datasets/imagenet extra/datasets/"
+su tiny -c "ln - /raid/weights ./"
+
 popd
