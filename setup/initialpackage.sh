475ef2bc833e6e702a3a6f27cd4a944a8af70612
Woze Parrot <wozeparrot@gmail.com>
2024-05-16 18:50:56 -0700

    feat: install pillow

--- setup/initialpackage.sh
+++ setup/initialpackage.sh
@@ -7,4 +7,5 @@ echo "atext,Preparing.. ,Preparing ..,Preparing. ." | nc -U /run/tinybox-screen.
 su tiny -c "git clone https://github.com/tinygrad/tinygrad /home/tiny/tinygrad"
 pushd /home/tiny/tinygrad
 su tiny -c "pip install -e ."
+su tiny -c "pip install pillow"
 popd
