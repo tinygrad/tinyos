7331fa41181a130946d9139e2e05c22621af8d9e
Woze Parrot <wozeparrot@gmail.com>
2024-05-16 17:06:34 -0700

    feat: install into venv

--- setup/ensuredspackage.sh
+++ setup/ensuredspackage.sh
@@ -4,6 +4,10 @@ set -x
 # Check which gpus are installed
 IS_NVIDIA_GPU=$(lspci | grep -i nvidia)
 
+source /opt/tinybox/build/venv/bin/activate
+
 if [ -n "$IS_NVIDIA_GPU" ]; then
   pip install nvidia-ml-py
 fi
+
+deactivate
