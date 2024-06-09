7fe024f842f8007f30975e3ce89f6f20d880bd94
Woze Parrot <wozeparrot@gmail.com>
2024-06-09 01:39:51 -0700

    feat: only disable for nvidia

--- setup/driverinstall.sh
+++ setup/driverinstall.sh
@@ -35,18 +35,26 @@ EOF
 else
   echo "NVIDIA GPU found."
   # Install NVIDIA drivers
-  pushd /tmp
 
   wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
   dpkg -i cuda-keyring_1.1-1_all.deb
   apt update -y
 
   # grab the patched p2p driver source
+  pushd /tmp
   curl -o driver.deb -L "https://github.com/tinygrad/open-gpu-kernel-modules/releases/download/$NVIDIA_DRIVER_VERSION-p2p/nvidia-kernel-source-550-open-0ubuntu1_amd64.deb"
   dpkg -i driver.deb
+  popd
 
   # install cuda
   apt install cuda-toolkit-12-4 nvidia-driver-550-open cuda-drivers-550 -y
 
-  popd
+  # disable unattended-upgrades for nvidia driver
+  cat <<EOF > /etc/apt/apt.conf.d/50unattended-upgrades
+Unattended-Upgrade::Package-Blacklist {
+  "nvidia-*";
+  "libnvidia-*";
+};
+EOF
+
 fi
