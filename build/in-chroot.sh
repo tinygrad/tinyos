c81a198d6d75e54fc744a8dd0e800b938e189fa8
Woze Parrot <wozeparrot@gmail.com>
2024-06-11 11:59:57 -0700

    fix: how is that still there

--- build/in-chroot.sh
+++ build/in-chroot.sh
@@ -17,7 +17,7 @@ ln -s /opt/tinybox/tools/power-limit_completion.sh /etc/bash_completion.d/
 
 # symlink service files
 ln -s /opt/tinybox/setup/secondboot.service /etc/systemd/system/
-ln -s /opt/tinynbox/service/autoupdate-tinybox.service /etc/systemd/system/
+ln -s /opt/tinybox/service/autoupdate-tinybox.service /etc/systemd/system/
 ln -s /opt/tinybox/setup/provision.service /etc/systemd/system/
 ln -s /opt/tinybox/service/buttonservice.service /etc/systemd/system/
 ln -s /opt/tinybox/service/displayservice.service /etc/systemd/system/
