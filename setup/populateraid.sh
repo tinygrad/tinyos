5ff36bfc9f529754f46d71b9abc13f4534398be9
Woze Parrot <wozeparrot@gmail.com>
2024-06-11 19:43:38 -0700

    feat: tcp/ip perf tuning

--- setup/populateraid.sh
+++ setup/populateraid.sh
@@ -2,8 +2,22 @@
 
 sleep 1
 
+# TCP/IP performance tuning
+sysctl net.ipv4.tcp_timestamps=0
+sysctl net.ipv4.tcp_sack=1
+sysctl net.core.netdev_max_backlog=250000
+sysctl net.core.rmem_max=4194304
+sysctl net.core.wmem_max=4194304
+sysctl net.core.rmem_default=4194304
+sysctl net.core.wmem_default=4194304
+sysctl net.core.optmem_max=4194304
+sysctl net.ipv4.tcp_rmem="4096 87380 4194304"
+sysctl net.ipv4.tcp_wmem="4096 65536 4194304"
+sysctl net.ipv4.tcp_low_latency=1
+
 ip ad add 10.0.0.2/24 dev enp65s0f0np0
 ip link set enp65s0f0np0 up
+ip link set enp65s0f0np0 mtu 9000
 rclone copy --config /opt/tinybox/setup/rclone.conf -P --copy-links --multi-thread-streams 32 --transfers 32 tinyd:/ /raid/ | while read -r line; do
   case "$line" in
     *ETA*)
