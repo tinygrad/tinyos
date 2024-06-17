63d3391e4f3f19738b6eda85e54fcf50452e911e
Woze Parrot <wozeparrot@gmail.com>
2024-06-17 16:26:16 -0700

    feat: better populate

---
+++ setup/parallel-rsync/chunk.py
@@ -0,0 +1,29 @@
+import sys, time
+
+TMPDIR = sys.argv[1]
+PARALLEL_RSYNC = int(sys.argv[2])
+
+with open(f"{TMPDIR}/files.all", "r") as f:
+  files = f.readlines()
+
+# format is "fsize fname"
+files = [(int(f.split(" ")[0]), " ".join(f.split(" ")[1:]).strip()) for f in files]
+
+# put file in the chunk with the smallest size
+chunks = [[0, []] for _ in range(PARALLEL_RSYNC)]
+st = time.monotonic()
+for i, f in enumerate(files):
+  min_chunk = min(chunks, key=lambda x: x[0])
+  min_chunk[1].append(f[1])
+  min_chunk[0] += f[0]
+
+  if i % 25000 == 0: print(f"\033[0;32mINFO: {i} of {len(files)} ({int(time.monotonic() - st)}s)\033[0m")
+
+# write chunks
+for i, c in enumerate(chunks):
+  with open(f"{TMPDIR}/chunk.{i}", "w") as f:
+    f.write("\n".join(c[1]) + "\n")
+
+# write the total size of all files
+with open(f"{TMPDIR}/total.size", "w") as f:
+  f.write(str(sum([f[0] for f in files])))
--- setup/populateraid.sh
+++ setup/populateraid.sh
@@ -19,19 +19,33 @@ sysctl net.ipv4.tcp_congestion_control=bbr
 ip ad add 10.0.0.2/24 dev enp65s0f0np0
 ip link set enp65s0f0np0 up
 ip link set enp65s0f0np0 mtu 9000
-rclone copy --config /opt/tinybox/setup/rclone.conf -P --copy-links --multi-thread-streams 32 --transfers 32 tinyd:/ /raid/ | while read -r line; do
-  case "$line" in
-    *ETA*)
-      # extract transfer speed
-      speed=$(echo "$line" | grep -oP '\d+\.\d+ [kMG]Bytes/s')
-      # extract percentage
-      percentage=$(echo "$line" | grep -oP '\d+%,' | grep -oP '\d+')
-      # extract ETA
-      eta=$(echo "$line" | grep -oP '(\d+h\d+m\d+s)|(\d+m\d+s)|(\d+s)')
-      echo "text,Populating RAID,${speed},${percentage}% - ${eta}" | nc -U /run/tinybox-screen.sock
-      ;;
-  esac
+
+/opt/tinybox/setup/parallel-rsync/prsync -a --inplace rsync://10.0.0.1:2555/raid/ /raid/ &
+
+# grab the total size of all the files
+total_size=$(find /tmp -f -name "total.size" -exec cat {} \;)
+
+# watch the progress of all the rsyncs
+SECONDS=0
+while true; do
+  # calculate the total size of all the files transferred
+  transferred_size=$(df -B1 /raid | tail -n 1 | awk '{print $3}')
+  # calculate the percentage of files transferred
+  percentage=$(echo "scale=2; $transferred_size / $total_size * 100" | bc | cut -d. -f1)
+  # calculate the speed of the transfer
+  speed=$(echo "scale=2; $transferred_size / $SECONDS / 1024 / 1024" | bc | cut -d. -f1)
+  # calculate the ETA
+  eta=$(echo "scale=2; ($total_size - $transferred_size) / $speed" | bc | awk '{printf "%d:%02d:%02d", $1/3600, $1%3600/60, $1%60}')
+
+  echo "text,Populating RAID,${speed},${percentage}% - ${eta}" | nc -U /run/tinybox-screen.sock
+
+  if ! pgrep -f prsync; then
+    break
+  fi
 done
-sudo chown -R tiny:tiny /raid
+
+chown -R tiny:tiny /raid
+
+
 ip ad del 10.0.0.2/24 dev enp65s0f0np0
 echo "sleep" | nc -U /run/tinybox-screen.sock
