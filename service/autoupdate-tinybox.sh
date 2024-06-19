9cdc4f0fb9dee2b5590bdba2336f1e13d5bf76d1
Woze Parrot <wozeparrot@gmail.com>
2024-06-19 10:59:53 -0700

    feat: proper check

--- service/autoupdate-tinybox.sh
+++ service/autoupdate-tinybox.sh
@@ -5,7 +5,7 @@ set -x
 pushd /opt/tinybox || true
 
 changed=1
-git fetch -v --dry-run 2>&1 | grep -q -v "up to date" && changed=0
+git fetch -v --dry-run 2>&1 | grep -q "up to date" && changed=0
 
 if [ $changed -eq 1 ]; then
   git pull
