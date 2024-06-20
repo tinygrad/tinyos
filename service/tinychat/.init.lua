43cf975efe519157c0f803f76f9f38bb5ec6a94a
Woze Parrot <wozeparrot@gmail.com>
2024-06-20 13:26:50 -0700

    feat: need to use shared mem

--- service/tinychat/.init.lua
+++ service/tinychat/.init.lua
@@ -9,13 +9,38 @@ RELAY_HEADERS_TO_CLIENT = {
   "Referrer-Policy",
 }
 
-BACKEND_PID = nil
-LAST_BACKEND_USE = 0
+MEM = unix.mapshared(8000 * 8)
+LOCK = 0
+function Lock()
+  local ok, old = MEM:cmpxchg(LOCK, 0, 1)
+  if not ok then
+    if old == 1 then
+      old = MEM:xchg(LOCK, 2)
+    end
+    while old > 0 do
+      MEM:wait(LOCK, 2)
+      old = MEM:xchg(LOCK, 2)
+    end
+  end
+end
+
+function Unlock()
+  local old = MEM:fetch_add(LOCK, -1)
+  if old == 2 then
+    MEM:store(LOCK, 0)
+    MEM:wake(LOCK, 1)
+  end
+end
+
+BACKEND_PID = 1
+LAST_BACKEND_USE = 2
 
 local function startBackend()
   -- start up the backend server if it's not already running
-  if not BACKEND_PID then
-    local pid = unix.fork()
+  Lock()
+  local pid = MEM:load(BACKEND_PID)
+  if pid == 0 then
+    pid = unix.fork()
     if pid == 0 then
       local python_prog = assert(unix.commandv("python3"))
 
@@ -38,29 +63,35 @@ local function startBackend()
         "4",
       }, env)
     else
-      BACKEND_PID = pid
+      MEM:store(BACKEND_PID, pid)
     end
   end
+  Unlock()
 end
 
 function OnServerHeartbeat()
-  if BACKEND_PID then
-    if GetTime() - LAST_BACKEND_USE > 5 * 60 then
+  Lock()
+  local pid = MEM:load(BACKEND_PID)
+  if pid ~= 0 then
+    local last_time = MEM:load(LAST_BACKEND_USE)
+    if GetTime() - last_time > 5 * 60 then
       Log(kLogInfo, "killing backend server")
-      unix.kill(BACKEND_PID, unix.SIGTERM)
-      BACKEND_PID = nil
+      unix.kill(pid, unix.SIGTERM)
+      MEM:store(BACKEND_PID, 0)
     end
   end
+  Unlock()
 end
 
 function OnHttpRequest()
   local path = GetPath()
   Log(kLogInfo, "path: %s" % { path })
-  print(BACKEND_PID)
 
   -- forward all /v1 paths to the backend server
   if string.match(path, "^/v1/") then
-    LAST_BACKEND_USE = GetTime()
+    Lock()
+    MEM:store(LAST_BACKEND_USE, GetTime())
+    Unlock()
 
     -- if this is a completion request, start the backend server if it's not already running
     if string.match(path, "^/v1/chat/completions") then
