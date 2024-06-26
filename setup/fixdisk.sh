9815f619f383354f7a75a91fce586cea9350d2fa
Woze Parrot <wozeparrot@gmail.com>
2024-06-25 17:44:02 -0700

    feat: more fixdisk

--- setup/fixdisk.sh
+++ setup/fixdisk.sh
@@ -7,3 +7,6 @@ disk=$(echo "$part" | sed 's/[0-9]*$//')
 
 # fix the backup gpt header using sgdisk
 sgdisk -e "$disk"
+
+# fix other problems with gdisk
+echo -e "v\nw\ny\ny\ny\ny\ny\n" | gdisk "$disk"
