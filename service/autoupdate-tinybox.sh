14baa03060c8367935bbc38c7a12c8cfac44b171
Woze Parrot <wozeparrot@gmail.com>
2024-08-09 18:53:55 -0700

    feat: autoupdate tweaks from dev

--- service/autoupdate-tinybox.sh
+++ service/autoupdate-tinybox.sh
@@ -3,12 +3,17 @@ set -x
 
 pushd /opt/tinybox || true
 
+# get the git branch
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+
+# reset hard to the current branch not upstream
+git reset --hard HEAD
+
 changed=1
-git fetch -v --dry-run 2>&1 | grep -q "up to date" && changed=0
+git fetch -v --dry-run 2>&1 | grep "$current_branch" | grep -q "up to date" && changed=0
 
 if [ $changed -eq 1 ]; then
   git pull
-  systemctl daemon-reload
   systemctl stop displayservice
   systemctl stop buttonservice
   systemctl stop tinychat
@@ -26,11 +31,18 @@ stage_files=$(find /opt/tinybox/service/autoupdate/ -type f -name "*.sh" | sort 
 for stage_file in $stage_files; do
   stage=$(basename "$stage_file" | cut -d'-' -f1)
   if [ "$stage" -gt "$CURRENT_STAGE" ]; then
-    bash "$stage_file"
-    echo "$stage" > /etc/tinybox-update-stage
+    # run the stage
+    # and if it succeeds, update the current stage
+    if bash "$stage_file" ; then
+      echo "$stage" > /etc/tinybox-update-stage
+    else
+      echo "Failed to run stage $stage"
+      break
+    fi
   fi
 done
 
+systemctl daemon-reload
 systemctl start displayservice
 systemctl start buttonservice
 systemctl start tinychat
