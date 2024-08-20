b4229ef375b27b9c6113fc9450c92dad265286cb
Woze Parrot <wozeparrot@gmail.com>
2024-08-20 00:44:19 -0700

    feat: ensure reentrant

--- tools/revert_first_setup.sh
+++ tools/revert_first_setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-
 # go back to before first setup
+#
+sed -i '/bash \/opt\/tinybox\/setup\/firstsetup.sh/d' "$HOME"/.profile
 echo "bash /opt/tinybox/setup/firstsetup.sh" >> /home/tiny/.profile
+
 touch /home/tiny/.before_firstsetup
