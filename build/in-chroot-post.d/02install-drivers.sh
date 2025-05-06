881e982cf2e63ac0e8763a5e734e37b092501144
Woze Parrot <wozeparrot@gmail.com>
2025-05-06 11:55:53 -0700

    feat: 570.133.20

--- build/in-chroot-post.d/02install-drivers.sh
+++ build/in-chroot-post.d/02install-drivers.sh
@@ -15,14 +15,16 @@ elif [[ "$TINYBOX_COLOR" == "green" ]]; then
   pushd /tmp
 
   curl -o keyring.deb -L "https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb"
-  curl -o driver.deb -L "https://github.com/wozeparrot/open-gpu-kernel-modules/releases/download/570.124.06-p2p/nvidia-kernel-source-570-open-0ubuntu1_amd64.deb"
+  curl -o driver.deb -L "https://github.com/wozeparrot/open-gpu-kernel-modules/releases/download/570.133.20-p2p/nvidia-kernel-source-570-open-0ubuntu1_amd64.deb"
 
   dpkg -i keyring.deb
   dpkg -i driver.deb
 
+  apt-mark hold nvidia-kernel-source-570-open
+
   apt update -y
   apt install cuda-toolkit-12-8 nvidia-driver-570-open cuda-drivers-570 -y
-  apt-mark hold nvidia-driver-570-open nvidia-dkms-570-open nvidia-kernel-common-570 nvidia-kernel-source-570-open cuda-drivers-570 cuda-toolkit-12-8 libnvidia-common-570
+  apt-mark hold nvidia-driver-570-open nvidia-dkms-570-open nvidia-kernel-common-570 cuda-drivers-570 cuda-toolkit-12-8 libnvidia-common-570
 
   popd
 else
