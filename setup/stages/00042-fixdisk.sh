37b2fdd475248983068c31a4f3f78a0ff9c52a4c
Woze Parrot <wozeparrot@gmail.com>
2025-05-11 20:39:38 -0700

    fix: fixdisk

--- setup/stages/00042-fixdisk.sh
+++ setup/stages/00042-fixdisk.sh
@@ -2,8 +2,13 @@
 set -xeo pipefail
 
 # find what disk we are on
-part=$(mount | grep 'on / ' | cut -d' ' -f1 | sed 's/p[0-9]*$//')
-disk=$(echo "$part" | sed 's/[0-9]*$//')
+part=$(mount | grep 'on / ' | cut -d' ' -f1)
+# get the disk name by removing the partition number
+if [[ "$part" == *"nvme"* ]]; then
+  disk=$(echo "$part" | sed 's/p[0-9]*$//')
+else
+  disk=$(echo "$part" | sed 's/[0-9]*$//')
+fi
 
 # fix the backup gpt header using sgdisk
 sgdisk -e "$disk"
