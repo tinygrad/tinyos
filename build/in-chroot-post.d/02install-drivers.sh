a9042b70130c78c1fb84d45d7c90695f07e9edef
Woze Parrot <wozeparrot@gmail.com>
2025-05-12 14:18:49 -0700

    feat: rocm 6.4

--- build/in-chroot-post.d/02install-drivers.sh
+++ build/in-chroot-post.d/02install-drivers.sh
@@ -6,8 +6,8 @@ source /etc/tinybox-release
 if [[ -z "$TINYBOX_CORE" ]]; then
   if [[ "$TINYBOX_COLOR" == "red" ]]; then
     wget -qO - https://repo.radeon.com/rocm/rocm.gpg.key | gpg --dearmor --output /etc/apt/keyrings/rocm.gpg
-    echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/rocm.gpg] https://repo.radeon.com/amdgpu/6.3.1/ubuntu noble main" | tee /etc/apt/sources.list.d/rocm.list
-    echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/rocm.gpg] https://repo.radeon.com/rocm/apt/6.3.1 noble main" | tee --append /etc/apt/sources.list.d/rocm.list
+    echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/rocm.gpg] https://repo.radeon.com/amdgpu/6.4/ubuntu noble main" | sudo tee /etc/apt/sources.list.d/rocm.list
+    echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/rocm.gpg] https://repo.radeon.com/rocm/apt/6.4 noble main" | sudo tee --append /etc/apt/sources.list.d/rocm.list
     echo -e 'Package: *\nPin: release o=repo.radeon.com\nPin-Priority: 600' | tee /etc/apt/preferences.d/rocm-pin-600
 
     apt update -y
