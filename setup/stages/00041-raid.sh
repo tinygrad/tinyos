414bf7ed830031897f4adaf8e97cb239be4488aa
Woze Parrot <wozeparrot@gmail.com>
2025-05-12 23:26:38 -0700

    feat: wipefs

--- setup/stages/00041-raid.sh
+++ setup/stages/00041-raid.sh
@@ -7,8 +7,9 @@ if [[ -z "$TINYBOX_CORE" ]]; then
   # see if a raid array is already created, checking for anything under /dev/md/
   if [ -z "$(ls -A /dev/md/)" ]; then
     echo "No RAID array found, creating one..."
+    wipefs -a /dev/nvme0n1 /dev/nvme1n1 /dev/nvme2n1 /dev/nvme3n1
     mdadm --zero-superblock /dev/nvme0n1 /dev/nvme1n1 /dev/nvme2n1 /dev/nvme3n1
-    mdadm --create /dev/md0 --level=0 --raid-devices=4 /dev/nvme0n1 /dev/nvme1n1 /dev/nvme2n1 /dev/nvme3n1 -f --run
+    mdadm --create /dev/md0 --level=0 --raid-devices=4 /dev/nvme0n1 /dev/nvme1n1 /dev/nvme2n1 /dev/nvme3n1 -f
     mkfs.ext4 /dev/md0
   else
     echo "RAID array found, skipping creation..."
