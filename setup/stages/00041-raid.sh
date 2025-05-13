13af7b40d2502f0713ff7e7eeab79e6d848e89ed
Woze Parrot <wozeparrot@gmail.com>
2025-05-12 23:41:12 -0700

    feat: more error checking

--- setup/stages/00041-raid.sh
+++ setup/stages/00041-raid.sh
@@ -4,19 +4,27 @@ set -x
 source /etc/tinybox-release
 
 if [[ -z "$TINYBOX_CORE" ]]; then
-  # see if a raid array is already created, checking for anything under /dev/md/
-  if [ -z "$(ls -A /dev/md/)" ]; then
+  # see if a raid array is already created, checking if there are any /dev/md*
+  if ! ls /dev/md* 1> /dev/null 2>&1; then
     echo "No RAID array found, creating one..."
+
+    # stop all md devices
+    mdadm --stop /dev/md*
     wipefs -a /dev/nvme0n1 /dev/nvme1n1 /dev/nvme2n1 /dev/nvme3n1
     mdadm --zero-superblock /dev/nvme0n1 /dev/nvme1n1 /dev/nvme2n1 /dev/nvme3n1
+
     mdadm --create /dev/md0 --level=0 --raid-devices=4 /dev/nvme0n1 /dev/nvme1n1 /dev/nvme2n1 /dev/nvme3n1 -f
-    mkfs.ext4 /dev/md0
+    mkfs.ext4 -F /dev/md0
   else
     echo "RAID array found, skipping creation..."
   fi
 
   # get the UUID of the RAID array, which is the uuid of the first md device which might not be md0
   UUID=$(blkid -s UUID -o value /dev/md* | head -n 1)
+  if [ -z "$UUID" ]; then
+    echo "Failed to get UUID of RAID array, exiting"
+    exit 1
+  fi
 
   # check if we already have a /raid mountpoint in fstab
   if ! grep -q "/raid" /etc/fstab; then
