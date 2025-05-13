0ac8ec6783835e375c77fb5b519a1a1c6a0962e4
Woze Parrot <wozeparrot@gmail.com>
2025-05-12 22:35:28 -0700

    fix: force create

--- setup/stages/00041-raid.sh
+++ setup/stages/00041-raid.sh
@@ -8,7 +8,7 @@ if [[ -z "$TINYBOX_CORE" ]]; then
   if [ -z "$(ls -A /dev/md/)" ]; then
     echo "No RAID array found, creating one..."
     mdadm --zero-superblock /dev/nvme0n1 /dev/nvme1n1 /dev/nvme2n1 /dev/nvme3n1
-    mdadm --create /dev/md0 --level=0 --raid-devices=4 /dev/nvme0n1 /dev/nvme1n1 /dev/nvme2n1 /dev/nvme3n1 -f
+    mdadm --create /dev/md0 --level=0 --raid-devices=4 /dev/nvme0n1 /dev/nvme1n1 /dev/nvme2n1 /dev/nvme3n1 -f --run
     mkfs.ext4 /dev/md0
   else
     echo "RAID array found, skipping creation..."
