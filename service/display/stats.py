7f8cb7aa9fbf7538b57c805db1f40cd62f39119a
Woze Parrot <wozeparrot@gmail.com>
2025-05-15 21:06:30 -0700

    feat: items

--- service/display/stats.py
+++ service/display/stats.py
@@ -115,19 +115,19 @@ class AMGPUStats(GPUStats):
     self._refresh()
 
     gpu_utilizations = []
-    for dev, metrics in self.metrics:
+    for dev, metrics in self.metrics.items():
       gpu_utilizations.append(self.ctx.get_gfx_activity(dev, metrics))
     return gpu_utilizations
 
   def get_gpu_memory_utilizations(self) -> list[float]:
     gpu_memory_utilizations = []
-    for dev, metrics in self.metrics:
+    for dev, metrics in self.metrics.items():
       gpu_memory_utilizations.append(self.ctx.get_mem_activity(dev, metrics))
     return gpu_memory_utilizations
 
   def get_gpu_power_draw(self) -> list[int]:
     gpu_power_draws = []
-    for dev, metrics in self.metrics:
+    for dev, metrics in self.metrics.items():
       gpu_power_draws.append(self.ctx.get_power(dev, metrics)[0])
     return gpu_power_draws
 
