fb7357e8e8a38add6dcc7d4e5b63893a5be001c3
Woze Parrot <wozeparrot@gmail.com>
2025-05-27 13:59:34 -0700

    fix: stop tinybox-display when reloading driver

--- setup/provision/trainresnet.sh
+++ setup/provision/trainresnet.sh
@@ -26,10 +26,10 @@ if [[ "$TINYBOX_COLOR" == "green" ]]; then
 
   export TRAIN_BEAM=4 IGNORE_JIT_FIRST_BEAM=1 BEAM_UOPS_MAX=1500 BEAM_UPCAST_MAX=64 BEAM_LOCAL_MAX=1024 BEAM_MIN_PROGRESS=10 BEAM_PADTO=0
 elif [[ "$TINYBOX_COLOR" == "red" ]]; then
-  export AMD=1
+  export AMD=1 AM_RESET=1 AMD_IFACE=pci
 
   # switch to am driver
-  display_wtext "unloading amdgpu"
+  sudo systemctl stop tinybox-display
   sleep 10 && sudo modprobe -r amdgpu
 
   # restart display to use am_smi
@@ -38,7 +38,6 @@ elif [[ "$TINYBOX_COLOR" == "red" ]]; then
   wait_for_display 10
   display status
 
-  export AM_RESET=1
   export GPUS=6 BS=1536 EVAL_BS=192
   export TRAIN_BEAM=4 IGNORE_JIT_FIRST_BEAM=1 BEAM_UOPS_MAX=2000 BEAM_UPCAST_MAX=96 BEAM_LOCAL_MAX=1024 BEAM_MIN_PROGRESS=5 BEAM_PADTO=0
 else
