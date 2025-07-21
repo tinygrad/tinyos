748d079c4c7049b8415752f529b1e3abb9fa585b
Woze Parrot <wozeparrot@gmail.com>
2025-07-21 15:14:56 -0700

    feat: bump expected time

--- setup/provision/trainresnet.sh
+++ setup/provision/trainresnet.sh
@@ -74,7 +74,7 @@ pkill -f monitortemps.sh
 if [[ "$TINYBOX_COLOR" == "green" ]]; then
   EXPECTED_TIME=13900
 elif [[ "$TINYBOX_COLOR" == "red" ]]; then
-  EXPECTED_TIME=14500
+  EXPECTED_TIME=14900
 else
   echo "Unknown tinybox color: $TINYBOX_COLOR"
   exit 1
