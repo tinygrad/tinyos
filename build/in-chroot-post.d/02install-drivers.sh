98a43902971079d1a194e594c0e89f8a6d4203c9
Woze Parrot <wozeparrot@gmail.com>
2025-07-21 15:47:53 -0700

    feat: always overwrite old key

--- build/in-chroot-post.d/02install-drivers.sh
+++ build/in-chroot-post.d/02install-drivers.sh
@@ -5,6 +5,7 @@ source /etc/tinybox-release
 
 if [[ -z "$TINYBOX_CORE" ]]; then
   if [[ "$TINYBOX_COLOR" == "red" ]]; then
+    rm -f /etc/apt/keyrings/rocm.gpg
     wget -qO - https://repo.radeon.com/rocm/rocm.gpg.key | gpg --dearmor --output /etc/apt/keyrings/rocm.gpg
     echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/rocm.gpg] https://repo.radeon.com/amdgpu/6.4.2/ubuntu noble main" | sudo tee /etc/apt/sources.list.d/rocm.list
     echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/rocm.gpg] https://repo.radeon.com/rocm/apt/6.4.2 noble main" | sudo tee --append /etc/apt/sources.list.d/rocm.list
