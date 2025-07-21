54d41c876f94c396945eab91b9b77987a7ca15cd
Woze Parrot <wozeparrot@gmail.com>
2025-07-21 11:42:51 -0700

    feat: tool to uninstall driver

---
+++ tools/uninstall_driver.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -xeo pipefail
+
+source /etc/tinybox-release
+
+if [[ -z "$TINYBOX_CORE" ]]; then
+  if [[ "$TINYBOX_COLOR" == "red" ]]; then
+    sudo apt remove --purge -y amdgpu-dkms rocm rocm-bandwidth-test
+  elif [[ "$TINYBOX_COLOR" == "green" ]]; then
+    sudo apt-mark unhold nvidia-driver-570-open nvidia-dkms-570-open nvidia-kernel-common-570 cuda-drivers-570 cuda-toolkit-12-8 libnvidia-common-570
+    sudo apt remove --purge -y cuda-toolkit-12-8 cuda-drivers-570
+    sudo apt remove --purge -y nvidia-driver-570-open
+    sudo apt remove --purge nvidia-compute-utils-570
+    sudo apt remove --purge nvidia-persistenced
+
+    sudo apt-mark unhold nvidia-kernel-source-570-open
+    sudo apt remove --purge -y nvidia-kernel-source-570-open
+  fi
+
+  sudo apt autoremove -y
+fi
