4a7e68c5f9969947ef5360ce1fff48cbe1e10d4b
Woze Parrot <wozeparrot@gmail.com>
2025-07-22 13:25:14 -0700

    fix: sudo

--- tools/update_driver.sh
+++ tools/update_driver.sh
@@ -6,8 +6,8 @@ source /etc/tinybox-release
 if [[ -z "$TINYBOX_CORE" ]]; then
   if [[ "$TINYBOX_COLOR" == "red" ]]; then
     # bump version in /etc/apt/sources.list.d/rocm.list
-    sed -i 's/6.3.3/6.4.2/g' /etc/apt/sources.list.d/rocm.list
-    sed -i 's/6.4.1/6.4.2/g' /etc/apt/sources.list.d/rocm.list
+    sudo sed -i 's/6.3.3/6.4.2/g' /etc/apt/sources.list.d/rocm.list
+    sudo sed -i 's/6.4.1/6.4.2/g' /etc/apt/sources.list.d/rocm.list
 
     sudo apt update -y
     sudo apt upgrade -y
