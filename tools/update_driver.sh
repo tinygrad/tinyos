d0798dd5dff065059e11fd9da3071948aa07ff09
Woze Parrot <wozeparrot@gmail.com>
2025-10-10 16:54:50 +0800

    feat: bump rocm to 7.0.1 script

--- tools/update_driver.sh
+++ tools/update_driver.sh
@@ -10,8 +10,17 @@ if [[ -z "$TINYBOX_CORE" ]]; then
     sudo sed -i 's/6.3.3/6.4.2/g' /etc/apt/sources.list.d/rocm.list
     sudo sed -i 's/6.4.1/6.4.2/g' /etc/apt/sources.list.d/rocm.list
 
+    if grep -q "6.4.2" /etc/apt/sources.list.d/rocm.list; then
+      sudo apt autoremove rocm rocm-core amdgpu-dkms -y
+      sudo cat <<EOF | sudo tee /etc/apt/sources.list.d/rocm.list
+deb [arch=amd64 signed-by=/etc/apt/keyrings/rocm.gpg] https://repo.radeon.com/rocm/apt/7.0.1 noble main
+deb [arch=amd64 signed-by=/etc/apt/keyrings/rocm.gpg] https://repo.radeon.com/graphics/7.0.1/ubuntu noble main
+deb [arch=amd64,i386 signed-by=/etc/apt/keyrings/rocm.gpg] https://repo.radeon.com/amdgpu/30.10.1/ubuntu noble main
+EOF
+    fi
+
     sudo apt update -y
-    sudo apt upgrade -y
+    sudo apt install rocm amdgpu-dkms -y
   elif [[ "$TINYBOX_COLOR" == "green" ]]; then
     echo "Unsupported Currently"
   fi
