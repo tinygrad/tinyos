fd2e34788a755144b5e1148dd51cfe4932008db0
George Hotz <72895+geohot@users.noreply.github.com>
2025-11-13 14:59:40 -0800

    Update PyTorch installation to nightly version

--- build/in-chroot-post.d/05install-torch__user.sh
+++ build/in-chroot-post.d/05install-torch__user.sh
@@ -8,7 +8,7 @@ if [[ -z "$TINYBOX_CORE" ]]; then
 
   # install pytorch
   if [[ "$TINYBOX_COLOR" == "red" ]]; then
-    pip install --user --break-system-packages torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm6.3
+    pip install --user --break-system-packages --pre torch torchvision --index-url https://download.pytorch.org/whl/nightly/rocm7.0
   elif [[ "$TINYBOX_COLOR" == "green" ]]; then
     pip install --user --break-system-packages torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu128
   fi
