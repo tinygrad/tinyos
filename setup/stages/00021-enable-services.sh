35ef739bd44f984083adfd8da2d28e7a7578eb61
Woze Parrot <wozeparrot@gmail.com>
2025-12-01 13:45:36 -0800

    feat: power limit red tinyboxes

--- setup/stages/00021-enable-services.sh
+++ setup/stages/00021-enable-services.sh
@@ -5,3 +5,7 @@ source /etc/tinybox-release
 
 systemctl enable autoupdate-tinybox
 systemctl enable tinybox-setup
+
+if [[ "$TINYBOX_COLOR" == "red" ]]; then
+  systemctl enable tinybox-red-powerlimit
+fi
