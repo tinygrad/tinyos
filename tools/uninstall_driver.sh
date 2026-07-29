29f567ceab839bcede548ec6ef757183edfe276f
Woze Parrot <wozeparrot@gmail.com>
2026-07-29 16:14:35 +0800

    fix: uninstall driver works on green

--- tools/uninstall_driver.sh
+++ tools/uninstall_driver.sh
@@ -7,15 +7,8 @@ if [[ -z "$TINYBOX_CORE" ]]; then
   if [[ "$TINYBOX_COLOR" == "red" ]]; then
     sudo apt remove --purge -y amdgpu-dkms rocm rocm-bandwidth-test
   elif [[ "$TINYBOX_COLOR" == "green" ]]; then
-    sudo apt-mark unhold nvidia-driver-570-open nvidia-dkms-570-open nvidia-kernel-common-570 cuda-drivers-570 cuda-toolkit-12-8 libnvidia-common-570
-    sudo apt remove --purge -y cuda-toolkit-12-8 cuda-drivers-570
-    sudo apt remove --purge -y nvidia-driver-570-open
-    sudo apt remove --purge -y nvidia-compute-utils-570
-    sudo apt remove --purge -y nvidia-persistenced
-    sudo apt remove --purge -y libnvidia-cfg1-570
-
-    sudo apt-mark unhold nvidia-kernel-source-570-open
-    sudo apt remove --purge -y nvidia-kernel-source-570-open
+    sudo apt-mark unhold nvidia-driver-570-open nvidia-dkms-570-open nvidia-kernel-common-570 nvidia-kernel-source-570-open cuda-drivers-570 cuda-toolkit-12-8 libnvidia-common-570
+    sudo apt remove --purge -y cuda-toolkit-12-8 cuda-drivers-570 nvidia-driver-570-open nvidia-compute-utils-570 nvidia-persistenced libnvidia-cfg1-570 nvidia-kernel-source-570-open
   fi
 
   sudo apt autoremove -y
